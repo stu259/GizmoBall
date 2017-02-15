3ed65b9a9b64fdb9f2e89ef0d604e2ae4dbb8867
Ross Clelland <Ross Clelland>
2017-02-15 16:57:17 +0000

    Added shapes to the board along with the ball except from triangle

--- src/view/FlipperBoard.java
+++ src/view/FlipperBoard.java
@@ -47,51 +47,67 @@ public  class FlipperBoard extends JPanel implements Observer {
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D) g;
-		IGizmo gizmo = new Gizmo (220,220);
-		int x =  gizmo.getStartX();
-		int y =  gizmo.getStartY();
-		gm.addGizmo("leftflipper",gizmo.getKey(),x,y);
-		g2.setColor(gizmo.getColor());
-		int width = 30;
-		g2.fillRect(x, y, 2*width, width/2);
-		
-		//stuff for the ball
-//		int x1 = 250;
-//		int y1 = 250;
-//		int vx = (int) ((Math.random() * 10.0) + 10.0);
-//		int vy = (int) ((Math.random() * 10.0) + 10.0);
-//		int radius = 6;
-//		Ball ball = new Ball(x1,y1,vx,vy);
-//		gm.addBall(ball);
-//		
-//        vx += (int) ((Math.random() * 10.0) - 5.0);
-//        vx = -vx;
-//        vy += (int) ((Math.random() * 10.0) - 5.0);
-//        vy = -vy;
-//		
-//        x += vx;
-//        if (x <= radius) {
-//            x = radius;
-//            vx = -vx;
-//        }
-//        if (x >= 500 - radius) {
-//            x = 500 - radius;
-//            vx = -vx;
-//        }
-//
-//        y += vy;
-//        if (y <= radius) {
-//            y = radius;
-//            vy = -vy;
-//        }
-//        if (y >= 500 - radius) {
-//            y = 500 - radius;
-//            vy = -vy;
-//        }
-//		g2.setColor(ball.getColor());
-//
-//		int width1 = 10;
-//		g2.fillOval(x1-radius, y1-radius, radius+radius, radius+radius);
+		//Left Flipper
+		IGizmo lf = new Gizmo (50,220);
+		int xlf =  lf.getStartX();
+		int ylf =  lf.getStartY();
+		gm.addGizmo("leftflipper",lf.getKey(),xlf,ylf);
+		g2.setColor(lf.getColor());
+		int widthlf = 30;
+		g2.fillRect(xlf, ylf, 2*widthlf, widthlf/2);
+
+		//Right Flipper
+		IGizmo rf = new Gizmo (375,220);
+		int xrf =  rf.getStartX();
+		int yrf =  rf.getStartY();
+		gm.addGizmo("rightflipper",rf.getKey(),xrf,yrf);
+		g2.setColor(rf.getColor());
+		int widthrf = 30;
+		g2.fillRect(xrf, yrf, 2*widthrf, widthrf/2);
+
+		//Square
+		IGizmo s = new Gizmo (220,400);
+		int xs =  s.getStartX();
+		int ys =  s.getStartY();
+		gm.addGizmo("square",s.getKey(),xs,ys);
+		g2.setColor(s.getColor());
+		int widths = 30;
+		g2.fillRect(xs, ys, widths, widths);
+
+//		//Triagnle
+//		IGizmo t = new Gizmo (100,400);
+//		int xt =  t.getStartX();
+//		int yt =  t.getStartY();
+//		gm.addGizmo("triangle",t.getKey(),xt,yt);
+//		g2.setColor(t.getColor());
+//		int widtht = 30;
+//		g2.fillPolygon();
+
+		//Circle
+		IGizmo c = new Gizmo (400,100);
+		int xc =  c.getStartX();
+		int yc =  c.getStartY();
+		gm.addGizmo("circle",c.getKey(),xc,yc);
+		g2.setColor(c.getColor());
+		int widthc = 30;
+		g2.fillOval(xc, yc, widthc, widthc);
+
+
+		int x1 = 250;
+		int y1 = 250;
+		int vx = (int) ((Math.random() * 10.0) + 10.0);
+		int vy = (int) ((Math.random() * 10.0) + 10.0);
+		int radius = 6;
+		Ball ball = new Ball(x1,y1,vx,vy);
+		gm.addBall(ball);
+
+        vx += (int) ((Math.random() * 10.0) - 5.0);
+        vx = -vx;
+        vy += (int) ((Math.random() * 10.0) - 5.0);
+        vy = -vy;
+
+		g2.setColor(ball.getColor());
+		g2.fillOval(x1-radius, y1-radius, radius+radius, radius+radius);
 		
 		
 	}
