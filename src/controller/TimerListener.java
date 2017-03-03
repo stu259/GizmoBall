8859e6edde039bbdcab694de2397468a011cece4
Douglas <douglas.keay.2014@uni.strath.ac.uk>
2017-03-03 11:42:23 +0000

    fixing build mode timer issue and tidying up some button code

---
+++ src/controller/TimerListener.java
@@ -0,0 +1,43 @@
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Timer;
+
+import model.IModel;
+
+public class TimerListener implements ActionListener {
+	Timer timer;
+	IModel model;
+
+	public TimerListener(IModel m) {
+		model=m;
+		timer = new Timer(50, this);
+	}
+
+	public void startTimer() {
+		timer.start();
+	}
+
+	public void stopTimer() {
+		timer.stop();
+	}
+
+	public Timer getTimer() {
+		return timer;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == timer) {
+			model.moveBalls();
+		} else if (e.getActionCommand().toLowerCase().equals("start")) {
+			timer.start();
+		} else if (e.getActionCommand().toLowerCase().equals("stop")) {
+			timer.stop();
+		}
+
+	}
+
+}
