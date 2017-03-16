77dca6dfc73cb1f9a79a28f39588c7290d40682d
Douglas Keay <qdb14203@uni.strath.ac.uk>
2017-03-16 16:04:56 +0000

    sliders change

--- src/view/Display.java
+++ src/view/Display.java
@@ -9,6 +9,7 @@ import java.awt.Toolkit;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyListener;
 import java.io.File;
+import java.util.Hashtable;
 import java.util.Map;
 
 import javax.swing.JFileChooser;
